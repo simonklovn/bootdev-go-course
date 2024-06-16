da57335de8112eb71c9ffa0c1baa7408c17b46b9
simonklovn <simon@veldig.travel>
2024-06-16 11:07:43 +0200

    Completed exercise 1 and 4

---
+++ 10-advanced-functions/1-first-class-and-higher-order-functions/go.mod
@@ -0,0 +1,3 @@
+module fist-class-and-higher-order-functions
+
+go 1.22.1
--- 10-advanced-functions/1-first-class-and-higher-order-functions/main.go
+++ 10-advanced-functions/1-first-class-and-higher-order-functions/main.go
@@ -1,6 +1,6 @@
 package main
 
-func getFormattedMessages(messages []string, formatter func) []string {
+func getFormattedMessages(messages []string, formatter func(string) string) []string {
 	formattedMessages := []string{}
 	for _, message := range messages {
 		formattedMessages = append(formattedMessages, formatter(message))
